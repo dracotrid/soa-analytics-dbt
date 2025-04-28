22b1493729b49311a9cc1106cc40c2705c5830e0
mihai <m.sorokin.uk@gmail.com>
2025-04-28 22:03:23 +0100

    SOA-12

--- models/data_sources/cleverbox/output/ds_cleverbox__service_sales.sql
+++ models/data_sources/cleverbox/output/ds_cleverbox__service_sales.sql
@@ -1,4 +1,6 @@
 WITH source AS (
+    SELECT * FROM {{ tf_source('ds_cleverbox__raw__service_sales_2024') }}
+    UNION ALL
     SELECT * FROM {{ tf_source('ds_cleverbox__raw__service_sales_2025') }}
 )
 
