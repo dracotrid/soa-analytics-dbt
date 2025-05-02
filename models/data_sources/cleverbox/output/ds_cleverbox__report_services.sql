81bccc530d504855a2659bd02817e7b4ee4ab1fb
mihai <m.sorokin.uk@gmail.com>
2025-05-02 02:02:22 +0100

    SOA-15

--- models/data_sources/cleverbox/output/ds_cleverbox__report_services.sql
+++ models/data_sources/cleverbox/output/ds_cleverbox__report_services.sql
@@ -6,10 +6,21 @@ WITH employees_speciality AS (
     GROUP BY name, job_title
 ),
 
+services AS (
+    SELECT
+        guid,
+        category AS service_category,
+        direction AS service_direction
+    FROM {{ tf_ref('ds_cleverbox__services') }}
+),
+
 source AS (
     SELECT *
     FROM {{ tf_ref('ds_cleverbox__service_sales') }} AS service_sales
     LEFT JOIN employees_speciality
         ON service_sales.specialist = employees_speciality.employees_speciality_name
+    LEFT JOIN services
+        ON service_sales.code = services.guid
 )
+
 {{ tf_transform_model('source') }}
