f89bd7bdd31003ec7facc265b30aff08acb8f658
mihai <m.sorokin.uk@gmail.com>
2025-05-13 10:07:59 +0100

    SOA-15

---
+++ models/data_sources/cleverbox/intermediate/ds_cleverbox__intermediate_report_services.sql
@@ -0,0 +1,27 @@
+WITH employees_speciality AS (
+    SELECT
+        name AS employees_speciality_name,
+        job_title AS speciality
+    FROM {{ tf_ref('ds_cleverbox__employees') }}
+    GROUP BY name, job_title
+),
+
+services AS (
+    SELECT
+        guid,
+        category AS service_category,
+        direction AS service_direction,
+        cost_price AS service_cost_price
+    FROM {{ tf_ref('ds_cleverbox__services') }}
+),
+
+source AS (
+    SELECT *
+    FROM {{ tf_ref('ds_cleverbox__service_sales') }} AS service_sales
+    LEFT JOIN employees_speciality
+        ON service_sales.specialist = employees_speciality.employees_speciality_name
+    LEFT JOIN services
+        ON LPAD(CAST(service_sales.code AS STRING), 6, '0') = services.guid
+)
+
+{{ tf_transform_model('source') }}
--- models/data_sources/cleverbox/output/ds_cleverbox__report_services.sql
+++ models/data_sources/cleverbox/output/ds_cleverbox__report_services.sql
@@ -1,5 +1,5 @@
 WITH source AS (
-    SELECT * FROM {{ tf_ref('ds_cleverbox__intermediate_step_2_report_services') }}
+    SELECT * FROM {{ tf_ref('ds_cleverbox__intermediate_income_report_services') }}
 )
 
 {{ tf_transform_model('source') }}
