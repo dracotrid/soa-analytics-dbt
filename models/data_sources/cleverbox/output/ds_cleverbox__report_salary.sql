2c71e19e267d5528814bff7bff44c8aedecde669
mihai <m.sorokin.uk@gmail.com>
2025-05-26 18:46:22 +0100

    SOA-30

---
+++ models/data_sources/cleverbox/output/ds_cleverbox__report_salary.sql
@@ -0,0 +1,29 @@
+WITH source AS (
+    SELECT
+        id,
+        'Послуга' AS product_type,
+        date,
+        branch,
+        specialist,
+        service_name AS product_name,
+        bonus_total AS bonus,
+        amount,
+        bonus_cleverbox_total AS bonus_cleverbox
+    FROM {{ tf_ref('ds_cleverbox__report_services') }}
+
+    UNION ALL
+
+    SELECT
+        id,
+        'Товар' AS product_type,
+        date,
+        branch,
+        specialist,
+        goods_name AS product_name,
+        bonus_total AS bonus,
+        amount,
+        cleverbox_bonus_total AS bonus_cleverbox
+    FROM {{ tf_ref('ds_cleverbox__report_goods') }}
+)
+
+{{ tf_transform_model('source') }}
