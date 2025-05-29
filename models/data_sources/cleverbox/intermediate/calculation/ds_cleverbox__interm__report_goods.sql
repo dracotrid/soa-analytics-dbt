5c7b800f0dd06851f80424149c65d5729fa4c024
SorokinMM <4dddd@ukr.net>
2025-05-29 18:52:19 +0100

    SOA-28: Fixed metrics discrepancy between DBT and GSheet

--- models/data_sources/cleverbox/intermediate/calculation/ds_cleverbox__interm__report_goods.sql
+++ models/data_sources/cleverbox/intermediate/calculation/ds_cleverbox__interm__report_goods.sql
@@ -18,19 +18,19 @@ bonus_report_goods AS (
 certificates_balance AS (
     SELECT
         yuid AS certificates_balance_id,
-        sum AS certificates_balance_sum
+        sum(sum) AS certificates_balance_sum
     FROM {{ tf_source('ds_cleverbox__raw__certificates_and_balance_goods') }}
-    GROUP BY yuid, sum
+    GROUP BY yuid
 ),
 
 report_goods_step_1 AS (
     SELECT
         *,
-        COALESCE(cost, 0) * COALESCE(amount, 0) AS cost_total,
-        COALESCE(cost_price_unit, 0) * COALESCE(amount, 0) AS calculated_cost_price_total,
-        COALESCE(cost, 0) - COALESCE(price, 0) AS discount_unit,
-        COALESCE(price, 0) * COALESCE(amount, 0) AS full_income_total,
-        COALESCE(price, 0) - COALESCE(cost_price_unit, 0) - COALESCE(bonus_unit, 0) AS profit_unit
+        coalesce(cost, 0) * coalesce(amount, 0) AS cost_total,
+        coalesce(cost_price_unit, 0) * coalesce(amount, 0) AS calculated_cost_price_total,
+        coalesce(cost, 0) - coalesce(price, 0) AS discount_unit,
+        coalesce(price, 0) * coalesce(amount, 0) AS full_income_total,
+        coalesce(price, 0) - coalesce(cost_price_unit, 0) - coalesce(bonus_unit, 0) AS profit_unit
     FROM {{ tf_ref('ds_cleverbox__prepared__goods_sales') }} AS goods_sales
     LEFT JOIN employees_speciality
         ON goods_sales.employee = employees_speciality.employees_speciality_name
@@ -43,16 +43,16 @@ report_goods_step_1 AS (
 report_goods_step_2 AS (
     SELECT
         *,
-        discount_unit * COALESCE(amount, 0) AS discount_total,
-        full_income_total - COALESCE(certificates_balance_sum, 0) AS income_total,
-        ROUND(CASE WHEN profit_unit = 0 THEN 0 ELSE COALESCE(price, 0) / profit_unit END, 2) AS payback
+        discount_unit * coalesce(amount, 0) AS discount_total,
+        full_income_total - coalesce(certificates_balance_sum, 0) AS income_total,
+        round(CASE WHEN profit_unit = 0 THEN 0 ELSE coalesce(price, 0) / profit_unit END, 2) AS payback
     FROM report_goods_step_1
 ),
 
 report_goods_step_3 AS (
     SELECT
         *,
-        income_total - calculated_cost_price_total - COALESCE(bonus_total, 0) AS profit_total
+        coalesce(income_total, 0) - coalesce(calculated_cost_price_total, 0) - coalesce(bonus_total, 0) AS profit_total
     FROM report_goods_step_2
 ),
 
