4f245527e469e2ddc7e075b7e0e764f71ceb5255
mihai <m.sorokin.uk@gmail.com>
2025-06-19 20:15:40 +0100

    SOA-58

---
+++ models/data_sources/cleverbox/intermediate/prepared/ds_cleverbox__prepared__report_product_sales.sql
@@ -0,0 +1,127 @@
+WITH source AS (
+    SELECT
+        id AS eid,
+        'Послуга' AS product_type,
+        date,
+        branch,
+        specialist AS employee_name,
+        client_name,
+        service_name AS product_name,
+        direction,
+        category,
+        amount,
+        price,
+        cost_price_unit,
+        payback,
+        cost_total AS cost,
+        income_total AS income,
+        cost_price_total AS cost_price,
+        discount_total AS discount,
+        bonus_total AS bonus,
+        profit_total AS profit,
+        margin
+    FROM {{ tf_ref('ds_cleverbox__report_service_sales') }}
+
+    UNION ALL
+
+    SELECT
+        id AS eid,
+        'Товар' AS product_type,
+        date,
+        branch,
+        specialist AS employee_name,
+        client_name,
+        goods_name AS product_name,
+        'Товар' AS direction,
+        brand AS category,
+        amount,
+        price,
+        cost_price_unit,
+        payback,
+        cost_total AS cost,
+        income_total AS income,
+        cost_price_total AS cost_price,
+        discount_total AS discount,
+        bonus_total AS bonus,
+        profit_total AS profit,
+        margin
+    FROM {{ tf_ref('ds_cleverbox__report_goods_sales') }}
+
+    UNION ALL
+
+    SELECT
+        id AS eid,
+        'Абонемент' AS product_type,
+        date,
+        branch,
+        specialist AS employee_name,
+        client AS client_name,
+        subscription_name AS product_name,
+        direction,
+        category,
+        amount,
+        price,
+        cost_price_unit,
+        payback,
+        cost_total AS cost,
+        income_total AS income,
+        cost_price_total AS cost_price,
+        discount_total AS discount,
+        bonus_total AS bonus,
+        profit_total AS profit,
+        margin
+    FROM {{ tf_ref('ds_cleverbox__report_subscription_sales') }}
+
+    UNION ALL
+
+    SELECT
+        id AS eid,
+        'Сертифікат' AS product_type,
+        date,
+        branch,
+        specialist AS employee_name,
+        client AS client_name,
+        certificate_name AS product_name,
+        direction,
+        category,
+        amount,
+        price,
+        cost_price_unit,
+        payback,
+        cost_total AS cost,
+        income_total AS income,
+        cost_price_total AS cost_price,
+        discount,
+        bonus_total AS bonus,
+        profit_total AS profit,
+        margin
+    FROM {{ tf_ref('ds_cleverbox__report_certificate_sales') }}
+
+    UNION ALL
+
+    SELECT
+        id AS eid,
+        'Баланс' AS product_type,
+        date,
+        branch,
+        specialist AS employee_name,
+        client AS client_name,
+        name AS product_name,
+        direction,
+        category,
+        amount,
+        price,
+        cost_price_unit,
+        payback,
+        cost_total AS cost,
+        income_total AS income,
+        cost_price_total AS cost_price,
+        discount,
+        bonus_total AS bonus,
+        profit_total AS profit,
+        margin
+    FROM {{ tf_ref('ds_cleverbox__report_balances') }}
+
+)
+
+{{ tf_transform_model('source') }}
