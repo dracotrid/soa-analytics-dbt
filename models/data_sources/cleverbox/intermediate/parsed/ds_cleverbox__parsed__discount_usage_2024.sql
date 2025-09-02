8f606478f9757be22f4fee9bc513a3bf7cecc2b2
SorokinMM <mihail.m.sorokin@gmail.com>
2025-09-02 09:59:10 +0100

    SOA-73: BigQuery + CSV on GCStorage

--- models/data_sources/cleverbox/intermediate/parsed/ds_cleverbox__parsed__discount_usage_2024.sql
+++ models/data_sources/cleverbox/intermediate/parsed/ds_cleverbox__parsed__discount_usage_2024.sql
@@ -1,5 +1,5 @@
 WITH source AS (
-    SELECT * FROM {{ tf_source('ds_cleverbox__raw__discount_usage') }}
+    SELECT * FROM {{ tf_source('ds_cleverbox__raw__discount_usage_2024') }}
 )
 
 {{ tf_transform_model('source') }}
--- models/data_sources/cleverbox/intermediate/parsed/ds_cleverbox__parsed__goods_sales.sql
+++ models/data_sources/cleverbox/intermediate/parsed/ds_cleverbox__parsed__goods_sales.sql
@@ -1,7 +1,7 @@
 WITH source AS (
     SELECT * FROM {{ tf_ref('ds_cleverbox__parsed__goods_sales_2024') }}
     UNION ALL
-    SELECT * FROM {{ tf_ref('ds_cleverbox__parsed__goods_sales_2025') }}
+    SELECT * FROM {{ tf_ref('ds_cleverbox__parsed__goods_sales_v1') }}
 )
 
 {{ tf_transform_model('source') }}
