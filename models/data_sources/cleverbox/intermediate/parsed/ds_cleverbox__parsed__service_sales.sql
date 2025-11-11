decd97558d0572a47f473616868405e2df0a23ae
SorokinMM <mihail.m.sorokin@gmail.com>
2025-11-11 20:28:48 +0000

    SOA-111

--- models/data_sources/cleverbox/intermediate/parsed/ds_cleverbox__parsed__service_sales.sql
+++ models/data_sources/cleverbox/intermediate/parsed/ds_cleverbox__parsed__service_sales.sql
@@ -1,6 +1,4 @@
 WITH source AS (
-    SELECT * FROM {{ tf_ref('ds_cleverbox__parsed__service_sales_2024') }}
-    UNION ALL
     SELECT * FROM {{ tf_ref('ds_cleverbox__parsed__service_sales_v1') }}
 )
 
